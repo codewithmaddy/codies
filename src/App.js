4cb26a6b358bd840d2fe2e126521ca8989d8625d
codewithmaddy <71321288+codewithmaddy@users.noreply.github.com>
2021-05-23 17:06:01 +0530

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src="https://cdn.dribbble.com/users/5270/screenshots/975617/meworking.gif"></img>
+      <h1> Grind is Still on </h1>
     </div>
   );
 }
